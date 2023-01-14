53057b7cfbb556bb12fd30afcb51cbb69ee889cf
Joel Pepper <pepper@bronze-deer.de>
2023-01-14 23:47:46 +0100

    add nix flake and direnv to autoload jsonnet tools
    if you have direnv and the nix packagemanager installed you can
    automatically load the relevant jsonnet tools when entering the
    directory

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system =  "x86_64-linux";
+    pkgs = import nixpkgs{inherit system;};
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+          packages = [
+            pkgs.go-jsonnet
+          ];
+      };
+    };
+  };
+}
